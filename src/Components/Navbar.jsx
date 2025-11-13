9e32f27d2101b4cd214243cf4aa3f8359a8aa99e
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-13 19:53:09 +0600

    navbar resoponsive complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,25 +5,25 @@ const Navbar = () => {
   return (
     <>
       <section id="navbar">
-        <nav className="py-5 sticky bg-[#000] ">
+        <nav className="py-5 sticky bg-[#000] px-4">
           <div className="container">
             <div className="nav-content flex items-center justify-between">
 
               {/* ----menu item--------- */}
               <div className="menuItems">
-                <button className="text-white text-5xl ">
+                <button className="text-white lg:text-5xl text-3xl ">
                   <TbMenu />
                 </button>
               </div>
 
               {/* -------logo part----- */}
-              <div className="logo text-white">
+              <div className="logo lg:w-44.5 w-25  lg:block text-white">
                 <img src={logo} alt="logoImg" />
               </div>
 
               {/* -----login button-------- */}
               <div className="button">
-                <button className="text-white border font-figtree font-bold py-2 px-6 rounded-full">
+                <button className="text-white border font-figtree py-1 px-2  lg:py-2 lg:px-6 rounded-full lg:text-base text-xs font-semibold">
                   {" "}
                   Login/Register{" "}
                 </button>
